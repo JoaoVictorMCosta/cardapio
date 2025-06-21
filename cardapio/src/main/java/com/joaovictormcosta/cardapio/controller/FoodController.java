0b23712add5c1293377cc099524de1262e17b085
João Victor M. Da Costa <108638735+JoaoVictorMCosta@users.noreply.github.com>
2025-06-21 17:48:29 +0000

    Add FoodController

---
+++ cardapio/src/main/java/com/joaovictormcosta/cardapio/controller/FoodController.java
@@ -0,0 +1,11 @@
+package com.joaovictormcosta.cardapio.controller;
+
+@RestCrontroller
+@RequestMapping("food")
+public class FoodController {
+    @GetMapping
+    public void getAll(){
+
+    }
+
+}
